4b1c52e7976ea25f67177861abba157dc8aa1a6d
Evgeny Kochetkov <evgeny.kochetkov@me.com>
2017-05-31 19:35:14 +0300

    don’t use inline styles

--- src/skylightstateless.jsx
+++ src/skylightstateless.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './styles';
-import assign from './utils/assign';
 
 export default class SkyLightStateless extends React.Component {
 
@@ -18,38 +16,35 @@ export default class SkyLightStateless extends React.Component {
   }
 
   render() {
-    const mergeStyles = key => assign({}, styles[key], this.props[key]);
     const { isVisible } = this.props;
-    const dialogStyles = mergeStyles('dialogStyles');
-    const overlayStyles = mergeStyles('overlayStyles');
-    const closeButtonStyle = mergeStyles('closeButtonStyle');
-    const titleStyle = mergeStyles('titleStyle');
-    overlayStyles.display = dialogStyles.display = 'block';
 
     let overlay;
     if (this.props.showOverlay) {
       overlay = (
-        <div className="skylight-overlay"
+        <div
+          className="skylight-overlay"
           onClick={() => this.onOverlayClicked()}
-          style={overlayStyles}
         />
       );
     }
 
     return isVisible ? (
-        <section className="skylight-wrapper">
-            {overlay}
-            <div className="skylight-dialog" style={dialogStyles}>
-              <a role="button" className="skylight-close-button"
-                onClick={() => this.onCloseClicked()}
-                style={closeButtonStyle}
-              >
-                &times;
-               </a>
-              <h2 style={titleStyle}>{this.props.title}</h2>
-              {this.props.children}
-            </div>
-        </section>
+      <section className="skylight-wrapper">
+        {overlay}
+        <div className="skylight-dialog">
+          <div className="skylight-dialog-header">
+            <a
+              role="button"
+              className="skylight-close-button"
+              onClick={() => this.onCloseClicked()}
+            >
+              &times;
+            </a>
+            <h2 className="skylight-title">{this.props.title}</h2>
+          </div>
+          {this.props.children}
+        </div>
+      </section>
     ) : <div />;
   }
 }
@@ -57,14 +52,10 @@ export default class SkyLightStateless extends React.Component {
 SkyLightStateless.displayName = 'SkyLightStateless';
 
 SkyLightStateless.sharedPropTypes = {
-  closeButtonStyle: PropTypes.object,
-  dialogStyles: PropTypes.object,
   onCloseClicked: PropTypes.func,
   onOverlayClicked: PropTypes.func,
-  overlayStyles: PropTypes.object,
   showOverlay: PropTypes.bool,
   title: PropTypes.string,
-  titleStyle: PropTypes.object,
 };
 
 SkyLightStateless.propTypes = {
@@ -75,7 +66,4 @@ SkyLightStateless.propTypes = {
 SkyLightStateless.defaultProps = {
   title: '',
   showOverlay: true,
-  overlayStyles: styles.overlayStyles,
-  dialogStyles: styles.dialogStyles,
-  closeButtonStyle: styles.closeButtonStyle,
 };
